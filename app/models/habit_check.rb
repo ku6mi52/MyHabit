616ecb516e1979361ad04680c61717cdedfad5d5
ku6mi52 <kurumi963design@gmail.com>
2025-09-30 01:53:10 +0900

    タスクチェックの一斉更新エラー修正

--- app/models/habit_check.rb
+++ app/models/habit_check.rb
@@ -1,17 +1,34 @@
 class HabitCheck < ApplicationRecord
-	belongs_to :daily_record
-	belongs_to :habit
-	
-	validates :habit_id, uniqueness: { scope: :daily_record_id }
-	
+  belongs_to :daily_record
+  belongs_to :habit
+
+  validates :habit_id, uniqueness: { scope: :daily_record_id }
+
   scope :checked, -> { where(done: true) }
 
-	def self.set_done!(user:, habit_id:, daily_record_id:, done:)
-		daily_record = user.daily_records.find_or_create_by!(recorded_on: date)
-		habit_check = find_or_create_by!(daily_record: daily_record, habit_id: habit_id)
-		habit_check.update!(done: ActiveModel::Type::Boolean.new.cast(done))
-		habit_check
-	end
+  def self.habit_checks_for_dashboard(user:, recorded_on:, checks: nil)
+    daily_record = user.daily_records.find_or_create_by!(recorded_on: recorded_on)
+
+    user.habits.order(:id).each do |habit|
+      daily_record.habit_checks.find_or_create_by!(habit: habit)
+    end
+
+    if checks.present?
+      submitted_checks = checks || {}
+
+      daily_record.habit_checks.find_each do |habit_check|
+        habit_id_key = habit_check.habit_id.to_s
+
+        per_habit_hash = submitted_checks[habit_id_key]
+        next if per_habit_hash.nil?
+
+        submitted_done_value = per_habit_hash["done"]
+        checked = (submitted_done_value == "1")
+
+        habit_check.update!(done: checked)
+      end
+    end
+  end
 
   def self.tasks_completion_rate(daily_records)
     Array(daily_records).map { |r|

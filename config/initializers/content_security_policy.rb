45271c344c2e4fa88aec8a4d2cd3f7cf18af903e
ku6mi52 <kurumi963design@gmail.com>
2025-10-09 08:18:21 +0900

    タブバー/ダッシュボードの画面

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,14 +4,14 @@
 # See the Securing Rails Applications Guide for more information:
 # https://guides.rubyonrails.org/security.html#content-security-policy-header
 
-# Rails.application.configure do
-#   config.content_security_policy do |policy|
-#     policy.default_src :self, :https
-#     policy.font_src    :self, :https, :data
-#     policy.img_src     :self, :https, :data
-#     policy.object_src  :none
-#     policy.script_src  :self, :https
-#     policy.style_src   :self, :https
+Rails.application.configure do
+  config.content_security_policy do |policy|
+  policy.default_src :self, :https
+  policy.font_src    :self, :https, "https://fonts.gstatic.com", :data
+  policy.img_src     :self, :https, :data
+  policy.object_src  :none
+  policy.script_src  :self, :https
+  policy.style_src   :self, :https, "https://fonts.googleapis.com"
 #     # Specify URI for violation reports
 #     # policy.report_uri "/csp-violation-report-endpoint"
 #   end

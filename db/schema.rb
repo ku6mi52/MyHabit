e2d49652914513452753e1bdee8150d90986ecc3
ku6mi52 <kurumi963design@gmail.com>
2025-10-17 15:43:55 +0900

    マイグレーション実行

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_10_01_173653) do
     t.float "body_fat_percentage"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "motivation", null: true
+    t.integer "motivation"
     t.index ["user_id", "recorded_on"], name: "index_daily_records_on_user_id_and_recorded_on", unique: true
     t.index ["user_id"], name: "index_daily_records_on_user_id"
   end

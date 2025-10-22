2971f037f2cc777c22deb341306801bc2b48513a
ku6mi52 <kurumi963design@gmail.com>
2025-10-22 22:24:35 +0900

    app/models/user.rbにdependent: :destroyを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :daily_records, dependent: :destroy
-  has_many :habits
+  has_many :habits, dependent: :destroy
 
 	with_options on: :onboarding_step1 do
     validates :start_weight, presence: true, numericality: { greater_than: 0, less_than: 200 }
